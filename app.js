fda54b1038262d136a40fe02172efceae861b8f7
Estudiante <luquiscardenas@gmail.com>
2025-05-12 01:01:52 -0300

    creacion del array y el registro que se mostrara en consolota

--- app.js
+++ app.js
@@ -24,3 +24,31 @@ switch (respuesta) {
   default:
     window.prompt("elegí 1 o 2...");
 }
+
+//apartado para el array
+
+//crear un objeto tras cada conversion
+
+//array de todo lo q hicimos, es una lista de los objetos que hay dentro de history
+
+let history = []; //este es el arreglo donde se va a guardar todo lo de los datos
+
+let registro = {
+  //"registro" es el objeto q se guarda dentro del arreglo "history"
+
+  tipo: "divisa",
+  entrada: "${pesos} pesos argentinos",
+  salida: "{dolares.toFixed(2)} dolares", //".toFixed(2)" es para redondear la conversion, q no muestre tanto los decimales
+  fecha: new Date().toLocaleDateString(),
+};
+
+//registro= cada q el usuario hace una conversion individual (cada q hace una conversion)
+//se guarda en registro
+//history= es una lista donde se guardn toddas las conversiones y lo muestra como
+//un array viendo todas las veces q hizo una conversion
+
+history.push(registro);
+//esto hace q se agregue la ultima conversion q se hizo
+//agrega nuevos elementos al final del array (en el history), no modifica el anterior sino q agrega lo nuevo
+
+console.log(history);
