9449d127bddfa4f4e82de0ad62f709e3bd3abe9a
Estudiante <luquiscardenas@gmail.com>
2025-05-11 21:53:32 -0300

    modificacion en la estructura inicial, se agrego un control de flujo y la funcion de conver

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-let pesos = window.prompt("ingrese la cantidad en pesos a cambair");
-let dolares;
-
-alert(
-  "la cantidad de" + pesos + "al convertir pasa a ser en dolares" + dolares
+let respuesta = window.prompt(
+  "Ingrese lo que quiera hacer. '1' Si quiere ver el valor de pesos a dolares '2' Si quiere salir"
 );
+const tipoCambio = 1100;
+
+//convertCurrency es una funcion q recibe un valor, lo transforma y devuelve otro
+//el dato q toma se pone dentro de los parentesis
+
+function convertCurrency(entrada) {
+  return entrada / tipoCambio;
+}
+
+switch (respuesta) {
+  case "1":
+    let pesos = window.prompt("ingrese la cantidad de pesos");
+
+    let dolares = convertCurrency(pesos);
+    console.log("el total en dolares seria: " + dolares);
+
+    break;
+  case "2":
+    console.log("bye bye!");
+
+  default:
+    console.log("elegí 1 o 2...");
+}
